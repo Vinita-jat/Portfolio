6b742b4fd913073d793430b77dd875bef12017a9
Vinita <vinitajat87@gmail.com>
2024-01-13 15:00:54 +0900

    initial commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="vinitajat87@gmail.com">vinitajat87@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/vinita-jat-2a523b193/">linkedin.com/VinitaJat</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Vinita-jat">https://github.com/Vinita-jat</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+export const Hero = () => {
+    return ( 
+    <section className={styles.container}>  
+        <div className={styles.content}>
+            <h1 className={styles.title}>Hello, I'm Vinita</h1>
+            <p className={styles.description}>I am a Full-stack Engineer, I
+             have over 2 years of experience in frontend, backend, and database side.
+             </p>
+             <a href="vinitajat87@gmail.com" className={styles.contactBtn}>Contact Me</a>
+        </div>
+        <img src={getImageUrl("hero/heroImage.png")} alt="Hero Image of me" className={styles.heroImg}/>
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+    </section>
+    );
+};
